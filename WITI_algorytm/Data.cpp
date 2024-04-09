854a3227cee8f6c5a018c42341dcaa3802459d74
Shoshano <shoshano2048@gmail.com>
2024-04-09 10:04:46 +0200

    klasa data

---
+++ WITI_algorytm/Data.cpp
@@ -0,0 +1,34 @@
+#include "Data.h"
+
+void Dane::GetData() {
+	number_of_data = 0;
+	std::string n;
+	std::fstream file;
+	file.open(file_path);
+	std::string line, chunk;
+	std::vector<std::string> tmp;
+	if (file.is_open()) {
+		getline(file, line);
+		number_of_data = stoi(line);
+		for (int i = 0; i < number_of_data; i++) {
+			Task d = Task(0, 0, 0);
+			getline(file, line);
+			std::stringstream s(line);
+			while (getline(s, chunk, ' '))
+			{
+				tmp.push_back(chunk);
+			}
+			d.get_Id = i + 1;
+			d.get_Tp = stoi(tmp.at(0));
+			d.get_W = stoi(tmp.at(1));
+			d.get_Tw = stoi(tmp.at(2));
+
+			list.push_back(d);
+			tmp.erase(tmp.begin(), tmp.end());
+		}
+	}
+	else {
+		std::cout << "Plik nie otwarty";
+	}
+}
+
---
+++ WITI_algorytm/Data.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Task.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <random>
+#include <time.h>
+#include <stdlib.h>
+
+class Data
+{
+	int number_of_data;
+	std::vector <Task> list;
+	std::string file_path;
+
+public:
+	void GetData();
+};
+
