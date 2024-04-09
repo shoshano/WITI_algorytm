950695b23d226acd20d38165501e43aa57fa6bf9
MaciekDanik <maciekdanik@gmail.com>
2024-04-09 10:47:50 +0200

    fileNames

--- WITI_algorytm/WITI_algorytm.cpp
+++ WITI_algorytm/WITI_algorytm.cpp
@@ -1,20 +1,27 @@
-﻿// WITI_algorytm.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include "Result.h"
+#include "Data.h"
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<string> FileNames;
+    string name = "data";
+    string exension = ".txt";
+
+    for (int i = 10; i < 21; i++)
+    {
+        string tmp = to_string(i);
+        string tmpName= name + tmp;
+        FileNames.push_back(tmpName);
+    }
+
+    for (auto x : FileNames)
+    {
+        cout << x << "\n";
+    }
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
